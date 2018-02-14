afedc2181af6ff2b185d474e8447cebe044a18a2
Kyle Teague <kyteague@gmail.com>
2018-02-14 11:46:06 +0700

    use utf8 byte length instead of length for encoding strings

--- js-wire/writer.js
+++ js-wire/writer.js
@@ -7,6 +7,19 @@ var assert = require('assert');
   ...
 */
 
+// returns the byte length of a utf8 string
+// modern browsers: (new TextEncoder('utf-8').encode('foo')).length
+function byteLength(str) {
+  const s = str.length;
+  for (let i = str.length - 1; i >= 0; i--) {
+    const code = str.charCodeAt(i);
+    if (code > 0x7f && code <= 0x7ff) s++;
+    else if (code > 0x7ff && code <= 0xffff) s += 2;
+    if (code >= 0xDC00 && code <= 0xDFFF) i--; // trail surrogate
+  }
+  return s;
+}
+
 var Writer = function(buf) {
   if (typeof buf == 'undefined') {
     buf = new Buffer(1024);
@@ -108,7 +121,7 @@ Writer.prototype.writeUvarint = function(v) {
 }
 
 Writer.prototype.writeString = function(v) {
-  this.writeUvarint(v.length);
+  this.writeUvarint(byteLength(v));
   var utf8Buf = new Buffer(v, 'utf8');
   if (this.buf.length - this.offset >= utf8Buf.length) {
     for (var i = 0; i < utf8Buf.length; i++) {
