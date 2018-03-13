bbca32543dd1490fb62ecf0ea4fd173688e61404
Kyle Teague <kyteague@gmail.com>
2018-03-13 22:10:59 +0700

    allow more flexible addresses

--- actor.js
+++ actor.js
@@ -1,10 +1,22 @@
 import { registerType } from './wire';
 
+function parseAddress(address) {
+    if (typeof address == 'string') {
+        if (address.startsWith('0x')) {
+            address = address.slice(2);
+        }
+
+        return new Buffer(address, 'hex')
+    }
+
+    return new Buffer(address);
+}
+
 export default class Actor {
     constructor(address, app = 'sigs', chainId = '') {
         this.chainId = chainId;
         this.app = app;
-        this.address = new Buffer(address);
+        this.address = parseAddress(address);
     }
 }
 
