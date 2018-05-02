9f7dc95d22b6c45b557babff83a771076d92e456
Vadim Macagon <vadim.macagon@gmail.com>
2018-05-02 14:10:59 +0700

    Fix handling of empty body in VaultClient.putAsync() response

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { KeyStoreFactory, IKeyStore, VaultStoreConfig } from './key-store-factory'
 export { Identity, IdentityProvider } from './identity-provider'
+import * as CryptoUtils from './crypto-utils'
+export { CryptoUtils }
