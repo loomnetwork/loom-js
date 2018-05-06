92d14f64e4bc05722409685612e1bf2227ba3290
Vadim Macagon <vadim.macagon@gmail.com>
2018-05-06 22:49:45 +0700

    Refactor the websocket code a bit

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,4 @@
-import { Client as RPCClient } from 'rpc-websockets'
+import { Client as WSClient } from 'rpc-websockets'
 import { Message } from 'google-protobuf'
 // import wretch from 'wretch'
 
@@ -27,22 +27,58 @@ export interface ITxMiddlewareHandler {
   Handle(txData: Readonly<Uint8Array>): Promise<Uint8Array>
 }
 
+interface IRPCClientOptions {
+  autoConnect?: boolean
+  reconnect?: boolean
+  reconnectInterval?: number
+  maxReconnects?: number
+  generateRequestId?: (method: string, params: object | any[]) => string | number
+}
+
+class RPCClient {
+  private _client: WSClient
+  private _openPromise?: Promise<void>
+
+  constructor(public url: string, opts: IRPCClientOptions = {}) {
+    this._client = new WSClient(url, {
+      autoconnect: opts.autoConnect,
+      reconnect: opts.reconnect,
+      reconnect_interval: opts.reconnectInterval,
+      max_reconnects: opts.maxReconnects
+    })
+  }
+
+  private _ensureConnectionAsync(): Promise<void> {
+    if (this._client.ready) {
+      return Promise.resolve()
+    }
+    if (!this._openPromise) {
+      this._openPromise = new Promise((resolve, reject) => {
+        this._client.on('open', () => resolve())
+        this._client.on('error', err => {
+          console.log(err)
+          reject(err)
+        })
+      })
+    }
+    return this._openPromise
+  }
+
+  async sendAsync<T>(method: string, params: object | any[]): Promise<T> {
+    await this._ensureConnectionAsync()
+    return this._client.call<T>(method, params)
+  }
+}
+
 /**
  * Writes to & reads from a Loom DAppChain.
  */
 export class Client {
   public readonly chainId: string
-
-  private _writeUrl: string
-  private _readUrl: string
-  private _rpcId = 0
-  private _openPromise?: Promise<void>
   private _writeClient: RPCClient
   private _readClient: RPCClient
 
   txMiddleware: ITxMiddlewareHandler[] = []
-
-  private _getNextRequestId = (): string => (++this._rpcId).toString()
 
   /**
    * Constructs a new client to read & write data from/to a Loom DAppChain.
@@ -52,32 +88,12 @@ export class Client {
    */
   constructor(chainId: string, writeUrl: string, readUrl?: string) {
     this.chainId = chainId
-    this._writeUrl = writeUrl
-    this._readUrl = readUrl || writeUrl
     // TODO: basic validation of the URIs to ensure they have all required components.
-    this._writeClient = new RPCClient(
-      this._writeUrl,
-      {
-        autoconnect: true,
-        reconnect: true,
-        reconnect_interval: 1000,
-        max_reconnects: 5
-      },
-      this._getNextRequestId
-    )
-    if (this._writeUrl === this._readUrl) {
+    this._writeClient = new RPCClient(writeUrl)
+    if (!readUrl || writeUrl === readUrl) {
       this._readClient = this._writeClient
     } else {
-      this._readClient = new RPCClient(
-        this._readUrl,
-        {
-          autoconnect: true,
-          reconnect: true,
-          reconnect_interval: 1000,
-          max_reconnects: 5
-        },
-        this._getNextRequestId
-      )
+      this._readClient = new RPCClient(readUrl)
     }
   }
 
@@ -93,8 +109,7 @@ export class Client {
       txBytes = await this.txMiddleware[i].Handle(txBytes)
     }
     const payload = Uint8ArrayToB64(txBytes)
-    const result = await this._sendMessageAsync<IBroadcastTxCommitResult>(
-      this._writeClient,
+    const result = await this._writeClient.sendAsync<IBroadcastTxCommitResult>(
       'broadcast_tx_commit',
       [payload]
     )
@@ -121,7 +136,7 @@ export class Client {
    * Queries the current state of a contract.
    */
   async queryAsync(contract: Address, query?: Message): Promise<Uint8Array | void> {
-    const result = await this._sendMessageAsync<string>(this._readClient, 'query', {
+    const result = await this._readClient.sendAsync<string>('query', {
       contract: contract.local.toString(),
       query: query ? query.serializeBinary() : undefined
     })
@@ -137,25 +152,6 @@ export class Client {
    * @return The nonce.
    */
   getNonceAsync(key: string): Promise<number> {
-    return this._sendMessageAsync<number>(this._readClient, 'nonce', { key })
-  }
-
-  private _ensureConnectionAsync(client: RPCClient): Promise<void> {
-    if (client.ready) {
-      return Promise.resolve()
-    }
-    if (!this._openPromise) {
-      this._openPromise = new Promise((resolve, reject) => {
-        client.on('open', () => {
-          resolve()
-        })
-      })
-    }
-    return this._openPromise
-  }
-
-  private async _sendMessageAsync<T>(client: RPCClient, method: string, params: any): Promise<T> {
-    await this._ensureConnectionAsync(client)
-    return client.call<T>(method, method, params)
+    return this._readClient.sendAsync<number>('nonce', { key })
   }
 }
--- src/types/rpcwebsockets.d.ts
+++ src/types/rpcwebsockets.d.ts
@@ -1,18 +1,21 @@
 // FIXME: For whatever reason TypeScript is unable to load these typings properly...
 declare module 'rpc-websockets' {
   interface IClientOptions {
-    autoconnect: boolean
-    reconnect: boolean
-    reconnect_interval: number
-    max_reconnects: number
+    autoconnect?: boolean
+    reconnect?: boolean
+    reconnect_interval?: number
+    max_reconnects?: number
   }
 
   export class Client {
+    ready: boolean
+
     constructor(
       address: string,
       options?: IClientOptions,
       generate_request_id?: (method: string, params: any) => string
     )
     call<T>(method: string, params?: any, timeout?: number, options?: any): Promise<T>
+    on(event: string, listener: (...args: any[]) => void): this
   }
 }
