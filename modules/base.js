60ed3da827abd373fd2200b8f3d269b1e0e74289
Vadim Macagon <vadim.macagon@gmail.com>
2018-05-11 19:43:33 +0700

    Remove more old code

--- modules/base.js
+++
@@ -1,9 +0,0 @@
-class ChainTx {
-    constructor(tx, chainId, expiresAt) {
-        this.tx = tx;
-        this.chainId = chainId;
-        this.expiresAt = expiresAt;
-    }
-}
-
-registerType(ChainTx, ['chainId', 'expiresAt', 'tx'], 0x03);
