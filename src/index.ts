936f43d6cd679b65b3a0329274aedae5cf1b11d5
Vadim Macagon <vadim.macagon@gmail.com>
2018-05-11 19:39:43 +0700

    Remove identity management stuff that was moved to loom-identity-js

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export { KeyStoreFactory, IKeyStore, VaultStoreConfig } from './key-store-factory'
-export { Identity, IdentityProvider } from './identity-provider'
 export { Client, ITxMiddlewareHandler } from './client'
 export { Contract } from './contract'
 export { Address, LocalAddress } from './address'
