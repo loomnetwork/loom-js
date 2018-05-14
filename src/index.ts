f26e89566121d56cb2e36c30dd583081b3e5bd3f
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2018-05-14 00:53:43 -0300

    Adding LoomProvider
    * Added web3js proto to call on DAppChain
    * Added compiling proto for new web3 proto

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { Client, ITxMiddlewareHandler } from './client'
 export { Contract } from './contract'
 export { Address, LocalAddress } from './address'
 export { SignedTxMiddleware, NonceTxMiddleware} from './middleware'
+export { LoomProvider } from './loom_provider'
 
 import * as CryptoUtils from './crypto-utils'
 export { CryptoUtils }
