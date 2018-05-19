a3a5049a12271275eca3c9fd175756f8dccd75d9
Vadim Macagon <vadim.macagon@gmail.com>
2018-05-19 18:27:23 +0700

    Make address unit test a bit more descriptive

--- src/tests/address_test.ts
+++ src/tests/address_test.ts
@@ -10,9 +10,10 @@ test('Address', t => {
     const addrStr = 'default:0x005B17864f3adbF53b1384F2E6f2120c6652F779'
 
     const addr = Address.fromString(addrStr)
-    t.equal(addr.chainId, chainId)
-    t.equal(addr.local, localAddr)
-    t.equal(addr.toString(), addrStr)
+    t.equal(addr.chainId, chainId, 'Address.fromString() returns correct chain ID')
+    t.deepEqual(addr.local, localAddr, 'Address.fromString() returns correct bytes')
+    // TODO: make this a case sensitive comparison
+    t.equal(addr.toString(), addrStr.toLowerCase(), 'Address.toString() formats the address correctly')
   } catch (err) {
     console.log(err)
   }
