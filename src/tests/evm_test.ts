d45f449e6c641ba65895a7ce39858341193dd146
sriep <piers.shepperson@btinternet.com>
2018-05-24 17:39:39 +0100

    Add unit test for new txReceiptAsync

--- src/tests/evm_test.ts
+++ src/tests/evm_test.ts
@@ -1,12 +1,29 @@
 import test from 'tape'
-
+import { EvmTxReceipt } from '../proto/loom_pb'
 import { EvmContract } from '../evm-contract'
 import { Address, LocalAddress } from '../address'
 import { Client } from '../client'
-import { generatePrivateKey, publicKeyFromPrivateKey } from '../crypto-utils'
+import { generatePrivateKey, publicKeyFromPrivateKey, bufferToProtobufBytes } from '../crypto-utils'
 import { NonceTxMiddleware, SignedTxMiddleware } from '../middleware'
 
-test('Contract Calls', async t => {
+/**
+ * Requires the SimpleStore solidity contract deployed on a loomchain.
+ * go-loom/examples/plugins/evmexample/contract/SimpleStore.sol
+ *
+ * pragma solidity ^0.4.18;
+ * contract SimpleStore {
+ *  function set(uint _value) public {
+ *   value = _value;
+ *  }
+ * function get() public constant returns (uint) {
+ *   return value;
+ * }
+ *  uint value;
+ * }
+ *
+ */
+
+test('EVM Contract Calls', async t => {
   try {
     const privKey = generatePrivateKey()
     const pubKey = publicKeyFromPrivateKey(privKey)
@@ -71,9 +88,9 @@ test('Contract Calls', async t => {
     const inputGetArray: number[] = [109, 76, 230, 60]
     const numRepeats = 10
     let results: Uint8Array[] = []
-
+    let rtv
     for (let i = 0; i < numRepeats; i++) {
-      let rtv = await evmContract.callAsync(inputSet987Array)
+      rtv = await evmContract.callAsync(inputSet987Array)
       if (rtv) {
         for (let result of results) {
           t.notDeepEqual(result, rtv, 'A different tx hash sould be returned' + ' each time')
@@ -81,6 +98,19 @@ test('Contract Calls', async t => {
         results.push(rtv)
       } else {
         t.fail('Should return a tx hash, check loomchain is running')
+      }
+    }
+
+    if (rtv) {
+      let rProto = await client.txReceiptAsync(rtv)
+      if (rProto) {
+        let reciept = EvmTxReceipt.deserializeBinary(bufferToProtobufBytes(rProto))
+        t.deepEqual(
+          Array.prototype.slice.call(reciept.getContractaddress_asU8(), 0),
+          contractAddr.local.bytes,
+          'Contract address should match'
+        )
+        t.equal(reciept.getStatus(), 1, 'Should return status 1 success')
       }
     }
 
