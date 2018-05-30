eae4509042dd28ebb463300da72365293d952b65
Vadim Macagon <vadim.macagon@gmail.com>
2018-05-30 13:48:56 +0700

    Tweak the protobuf generation script to try to get it working in Travis

--- scripts/gen-proto.js
+++ scripts/gen-proto.js
@@ -4,11 +4,17 @@ const shell = require('shelljs')
 const os = require('os')
 const path = require('path')
 
-shell.mkdir('-p', 'dist')
-shell.cd('src')
+shell.mkdir('-p', './dist')
 // copy the proto so end users can import it from node_modules in their own proto files
-shell.cp('proto/loom.proto', '../dist/proto/loom.proto')
+shell.cp('./src/proto/loom.proto', './dist/proto/loom.proto')
 
-const tsPluginPath = path.join('..', 'node_modules', '.bin',  os.platform() === 'win32' ? 'protoc-gen-ts.cmd' : 'protoc-gen-ts')
-const jsOutputPath = path.join('..', 'dist')
-shell.exec(`protoc --plugin=\"protoc-gen-ts=${tsPluginPath}\" --js_out=\"import_style=commonjs,binary:${jsOutputPath}/\" --ts_out=\".\" ./proto/loom.proto ./tests/tests.proto`)
+shell.pushd('./src')
+try {
+  const tsPluginPath = path.join('..', 'node_modules', '.bin',  os.platform() === 'win32' ? 'protoc-gen-ts.cmd' : 'protoc-gen-ts')
+  const jsOutputPath = path.join('..', 'dist')
+  shell.exec(`protoc --plugin=\"protoc-gen-ts=${tsPluginPath}\" --js_out=\"import_style=commonjs,binary:${jsOutputPath}/\" --ts_out=\".\" ./proto/loom.proto ./tests/tests.proto`)
+} catch (err) {
+  throw err
+} finally {
+  shell.popd()
+}
