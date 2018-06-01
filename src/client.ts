bd31b7c071c23dd56e7a9c0b6afc6a7b65c9d610
Vadim Macagon <vadim.macagon@gmail.com>
2018-06-01 21:44:01 +0700

    Expose socket events through the Client

--- src/client.ts
+++ src/client.ts
@@ -5,7 +5,12 @@ import retry from 'retry'
 import { VMType, EvmTxReceipt } from './proto/loom_pb'
 import { Uint8ArrayToB64, B64ToUint8Array, bufferToProtobufBytes } from './crypto-utils'
 import { Address, LocalAddress } from './address'
-import { WSRPCClient, IEventData } from './internal/ws-rpc-client'
+import {
+  WSRPCClient,
+  WSRPCClientEvent,
+  IJSONRPCEvent,
+  IJSONRPCError
+} from './internal/ws-rpc-client'
 
 interface ITxHandlerResult {
   code?: number
@@ -29,19 +34,57 @@ export interface ITxMiddlewareHandler {
 }
 
 export enum ClientEvent {
-  Contract = 'contractEvent'
+  /**
+   * Emitted when an event is received from a smart contract.
+   * Listener will receive IChainEventArgs.
+   */
+  Contract = 'contractEvent',
+  /**
+   * Emitted when an error occurs that can't be relayed by other means.
+   * Listener will receive IClientErrorEventArgs.
+   */
+  Error = 'error',
+  /**
+   * Emitted when a connection is established to the DAppChain.
+   * Listener will receive INetEventArgs.
+   */
+  Connected = 'connected',
+  /**
+   * Emitted when a connection with the DAppChain is closed.
+   * Listener will receive INetEventArgs.
+   */
+  Disconnected = 'disconnected'
+}
+
+export interface IClientEventArgs {
+  kind: ClientEvent
+  /** URL that corresponds to the RPC client this event originated from. */
+  url: string
 }
 
 /**
- * Generic event emitted by smart contracts.
+ * Event that's emitted when some kind of error occurs that can't be relayed by other means,
+ * e.g. socket error that occurs while listening for RPC events.
  */
-export interface IChainEventArgs {
-  // Address of the contract that emitted the event.
+export interface IClientErrorEventArgs extends IClientEventArgs {
+  kind: ClientEvent.Error
+  /** May contain additional information in case of an RPC error. */
+  error?: IJSONRPCError
+}
+
+/** Generic event containing data emitted by smart contracts. */
+export interface IChainEventArgs extends IClientEventArgs {
+  kind: ClientEvent.Contract
+  /** Address of the contract that emitted the event. */
   contractAddress: Address
-  // Address of the caller that caused the event to be emitted.
+  /** Address of the caller that caused the event to be emitted. */
   callerAddress: Address
+  /** The block containing the tx that caused this event to be emitted. */
   blockHeight: string
-  // The data emitted by the smart contract, the format and structure is defined by that contract.
+  /**
+   * Data that was actually emitted by the smart contract,
+   * the format and structure is defined by that contract.
+   */
   data: Uint8Array
 }
 
@@ -66,7 +109,6 @@ export function isInvalidTxNonceError(err: any): boolean {
  */
 export class Client extends EventEmitter {
   readonly chainId: string
-  readonly readUrl?: string
 
   private _writeClient: WSRPCClient
   private _readClient!: WSRPCClient
@@ -87,28 +129,57 @@ export class Client extends EventEmitter {
     randomize: true
   }
 
+  get readUrl(): string {
+    return this._readClient.url
+  }
+
+  get writeUrl(): string {
+    return this._writeClient.url
+  }
+
   /**
    * Constructs a new client to read & write data from/to a Loom DAppChain.
    * @param chainId DAppChain identifier.
    * @param writeUrl Host & port to send txs, specified as "<protocol>://<host>:<port>".
-   * @param readUrl Host & port of the DAppChain read/query interface.
+   * @param readUrl Host & port of the DAppChain read/query interface, this should only be provided
+   *                if it's not the same as `writeUrl`.
    */
   constructor(chainId: string, writeUrl: string, readUrl?: string) {
     super()
     this.chainId = chainId
     // TODO: basic validation of the URIs to ensure they have all required components.
     this._writeClient = new WSRPCClient(writeUrl)
+    this._writeClient.on(WSRPCClientEvent.Connected, () =>
+      this._emitNetEvent(writeUrl, ClientEvent.Connected)
+    )
+    this._writeClient.on(WSRPCClientEvent.Disconnected, () =>
+      this._emitNetEvent(writeUrl, ClientEvent.Disconnected)
+    )
+    this._writeClient.on(WSRPCClientEvent.Error, () =>
+      this._emitNetEvent(writeUrl, ClientEvent.Error)
+    )
+
     if (!readUrl || writeUrl === readUrl) {
       this._readClient = this._writeClient
     } else {
-      this.readUrl = readUrl
       this._readClient = new WSRPCClient(readUrl)
+      this._readClient.on(WSRPCClientEvent.Connected, () =>
+        this._emitNetEvent(readUrl, ClientEvent.Connected)
+      )
+      this._readClient.on(WSRPCClientEvent.Disconnected, () =>
+        this._emitNetEvent(readUrl, ClientEvent.Disconnected)
+      )
+      this._readClient.on(WSRPCClientEvent.Error, () =>
+        this._emitNetEvent(readUrl, ClientEvent.Error)
+      )
     }
 
-    const emitContractEvent = this._emitContractEvent.bind(this)
+    const emitContractEvent = (event: IJSONRPCEvent) =>
+      this._emitContractEvent(this._readClient.url, event)
 
     this.on('newListener', (event: string) => {
       if (event === ClientEvent.Contract && this.listenerCount(ClientEvent.Contract) === 0) {
+        // TODO: emit the error that occurs if sub fails
         this._readClient.subscribeAsync(emitContractEvent)
       }
     })
@@ -256,18 +327,35 @@ export class Client extends EventEmitter {
     return Address.fromString(addrStr)
   }
 
-  private _emitContractEvent(event: IEventData) {
-    this.emit(ClientEvent.Contract, {
-      contractAddress: new Address(
-        event.address.ChainID,
-        new LocalAddress(B64ToUint8Array(event.address.Local))
-      ),
-      callerAddress: new Address(
-        event.caller.ChainID,
-        new LocalAddress(B64ToUint8Array(event.caller.Local))
-      ),
-      blockHeight: event.blockHeight,
-      data: B64ToUint8Array(event.encodedData)
-    } as IChainEventArgs)
+  private _emitContractEvent(url: string, event: IJSONRPCEvent): void {
+    const { error, result } = event
+    if (error) {
+      const eventArgs: IClientErrorEventArgs = { kind: ClientEvent.Error, url, error }
+      this.emit(ClientEvent.Error, eventArgs)
+    } else if (result) {
+      const eventArgs: IChainEventArgs = {
+        kind: ClientEvent.Contract,
+        url,
+        contractAddress: new Address(
+          result.address.ChainID,
+          new LocalAddress(B64ToUint8Array(result.address.Local))
+        ),
+        callerAddress: new Address(
+          result.caller.ChainID,
+          new LocalAddress(B64ToUint8Array(result.caller.Local))
+        ),
+        blockHeight: result.blockHeight,
+        data: B64ToUint8Array(result.encodedData)
+      }
+      this.emit(ClientEvent.Contract, eventArgs)
+    }
+  }
+
+  private _emitNetEvent(
+    url: string,
+    kind: ClientEvent.Connected | ClientEvent.Disconnected | ClientEvent.Error
+  ) {
+    const eventArgs: IClientEventArgs = { kind, url }
+    this.emit(kind, eventArgs)
   }
 }
--- src/internal/ws-rpc-client.ts
+++ src/internal/ws-rpc-client.ts
@@ -14,34 +14,54 @@ export interface IEventData {
   encodedData: string
 }
 
-export type WSRPCClientEventListener = (eventArgs: IEventData) => void
+export interface IJSONRPCError {
+  code: number
+  message: string
+  data: any
+}
+
+export interface IJSONRPCEvent {
+  error?: IJSONRPCError
+  result?: IEventData
+}
+
+export type WSRPCClientEventListener = (event: IJSONRPCEvent) => void
+
+export enum WSRPCClientEvent {
+  /** Emitted when a connection is established with the server. */
+  Connected = 'connected',
+  /** Emitted when the connection to the server is closed down. */
+  Disconnected = 'disconnected',
+  /** Emitted when there's a connection error of some kind. */
+  Error = 'error',
+  /** Emitted when an event message (not a response) is received from the server. */
+  Message = 'message'
+}
 
 /**
  * Sends JSON-RPC messages via web sockets.
  */
-export class WSRPCClient {
+export class WSRPCClient extends EventEmitter {
   private _client: WSClient
   private _openPromise?: Promise<void>
   private _rpcId: number = 0
-  private _subscribers: Array<WSRPCClientEventListener> = []
 
   private _getNextRequestId = () => (++this._rpcId).toString()
 
   constructor(
     public url: string,
     opts: {
-      autoConnect?: boolean
       reconnect?: boolean
       reconnectInterval?: number
       maxReconnects?: number
       generateRequestId?: (method: string, params: object | any[]) => string
     } = {}
   ) {
-    this._onEventMessage = this._onEventMessage.bind(this)
+    super()
     this._client = new WSClient(
       url,
       {
-        autoconnect: opts.autoConnect,
+        autoconnect: true,
         reconnect: opts.reconnect,
         reconnect_interval: opts.reconnectInterval,
         max_reconnects: opts.maxReconnects
@@ -57,16 +77,27 @@ export class WSRPCClient {
     this._client.close(0)
   }
 
+  /**
+   * Waits for a connection to be established to the server (if it isn't already).
+   * @returns A promise that will be resolved when a connection is established.
+   */
   private _ensureConnectionAsync(): Promise<void> {
     if (this._client.ready) {
       return Promise.resolve()
     }
     if (!this._openPromise) {
       this._openPromise = new Promise((resolve, reject) => {
-        this._client.on('open', () => resolve())
+        this._client.once('open', () => {
+          this.emit(WSRPCClientEvent.Connected)
+          resolve()
+        })
+        this._client.on('close', () => {
+          this.emit(WSRPCClientEvent.Disconnected)
+          reject()
+        })
         this._client.on('error', err => {
-          console.log(err)
-          reject(err)
+          this.emit(WSRPCClientEvent.Error, err)
+          reject()
         })
       })
     }
@@ -87,43 +118,40 @@ export class WSRPCClient {
   }
 
   async subscribeAsync(listener: WSRPCClientEventListener): Promise<void> {
-    if (this._subscribers.indexOf(listener) !== -1) {
+    if (this.listeners(WSRPCClientEvent.Message).indexOf(listener) !== -1) {
       return
     }
-    this._subscribers.push(listener)
-    if (this._subscribers.length === 1) {
-      // rpc-websockets is just going to throw away the event messages from the DAppChain because
-      // they don't conform to it's idea of notifications or events... fortunately few things in
-      // javascript are truly private... so we'll just handle those event message ourselves ;)
-      ;((this._client as any).socket as EventEmitter).on('message', this._onEventMessage)
+    this.on(WSRPCClientEvent.Message, listener)
+    if (this.listenerCount(WSRPCClientEvent.Message) === 1) {
+      this._startSocketMsgIntercept()
     }
     await this.sendAsync('subevents', {})
   }
 
   async unsubscribeAsync(listener: WSRPCClientEventListener): Promise<void> {
-    const idx = this._subscribers.indexOf(listener)
-    if (idx !== -1) {
-      this._subscribers.splice(idx, 1)
-    }
-    if (this._subscribers.length === 0) {
-      ;((this._client as any).socket as EventEmitter).removeListener(
-        'message',
-        this._onEventMessage
-      )
+    this.removeListener(WSRPCClientEvent.Message, listener)
+    if (this.listenerCount(WSRPCClientEvent.Message) === 0) {
+      this._stopSocketMsgIntercept()
       await this.sendAsync('unsubevents', {})
     }
   }
 
-  private _onEventMessage(message: string | ArrayBuffer) {
-    let msgStr = message instanceof ArrayBuffer ? Buffer.from(message).toString() : message
+  private _onEventMessage = (message: string | ArrayBuffer): void => {
+    const msgStr = message instanceof ArrayBuffer ? Buffer.from(message).toString() : message
     const msg = JSON.parse(msgStr)
     if (msg.id === '0') {
-      if (msg.error) {
-        // TODO: should let end-users decide if this should be logged or not
-        console.log(`JSON-RPC Error ${msg.error.code} (${msg.error.message}): ${msg.error.data}`)
-      } else {
-        this._subscribers.forEach(listener => listener(msg.result))
-      }
+      this.emit(WSRPCClientEvent.Message, msg)
     }
+  }
+
+  private _startSocketMsgIntercept() {
+    // rpc-websockets is just going to throw away the event messages from the DAppChain because
+    // they don't conform to it's idea of notifications or events... fortunately few things in
+    // javascript are truly private... so we'll just handle those event message ourselves ;)
+    ;((this._client as any).socket as EventEmitter).on('message', this._onEventMessage)
+  }
+
+  private _stopSocketMsgIntercept() {
+    ;((this._client as any).socket as EventEmitter).removeListener('message', this._onEventMessage)
   }
 }
