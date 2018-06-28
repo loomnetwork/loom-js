4d8a52accdfbb7d73f779562655d22030193d1f3
Vadim Macagon <vadim.macagon@gmail.com>
2018-06-28 22:33:15 +0700

    Expand the EthereumPlasmaClient API

--- src/plasma-cash/ethereum-client.ts
+++ src/plasma-cash/ethereum-client.ts
@@ -1,4 +1,24 @@
+import BN from 'bn.js'
+import Web3 from 'web3'
+
+const web3 = new Web3()
+
 export class EthereumPlasmaClient {
-  startExit(slot: string) {}
-  finalizeExits() {}
+  private _plasmaContract: any // TODO: figure out how to type this properly
+
+  constructor(plasmaContractAddr: string) {
+    const plasmaABI = require('./contracts/plasma-cash-abi.json')
+    this._plasmaContract = new web3.eth.Contract(plasmaABI)
+  }
+
+  startExitAsync(params: { slot: BN; sendOpts: any }): Promise<any> {
+    return this._plasmaContract.methods.startExit(params.slot).send(params.sendOpts)
+  }
+
+  finalizeExitsAsync(params: { sendOpts: any }): Promise<any> {
+    return this._plasmaContract.methods.finalizeExits().send(params.sendOpts)
+  }
+
+  withdrawAsync() {}
+  withdrawBondsAsync() {}
 }
