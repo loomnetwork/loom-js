84b11ec14913eb14bb5ea63eed7fca455969a732
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2018-08-29 12:15:10 -0300

    Testing past events with ganache too

--- src/tests/e2e/loom-provider-web3-pastevents.ts
+++ src/tests/e2e/loom-provider-web3-pastevents.ts
@@ -1,4 +1,4 @@
-import test from 'tape'
+import test, { Test } from 'tape'
 
 import { LocalAddress, CryptoUtils } from '../../index'
 import { createTestClient, waitForMillisecondsAsync } from '../helpers'
@@ -6,119 +6,182 @@ import { createTestClient, waitForMillisecondsAsync } from '../helpers'
 import { LoomProvider } from '../../loom-provider'
 import { deployContract } from '../evm-helpers'
 
-// import Web3 from 'web3'
 const Web3 = require('web3')
 
 /**
  * Requires the SimpleStore solidity contract deployed on a loomchain.
  * go-loom/examples/plugins/evmexample/contract/SimpleStore.sol
  *
- * pragma solidity ^0.4.22;
+ * pragma solidity 0.4.24;
  *
- * contract SimpleStore {
- *   uint value;
- *   constructor() public {
- *       value = 10;
- *   }
+ * interface ContractB {
+ *   function callEvent(uint256 v) external;
+ * }
  *
- *   event NewValueSet(uint indexed _value);
+ * contract ContractA {
+ *   event ContractAEvent(uint256 v);
  *
- *   function set(uint _value) public {
- *     value = _value;
- *     emit NewValueSet(value);
- *   }
- *
- *   function get() public view returns (uint) {
- *     return value;
+ *   function doEmit(uint256 _v, address _contractBAddr) public {
+ *     emit ContractAEvent(_v);
+ *     ContractB(_contractBAddr).callEvent(_v);
  *   }
  * }
  *
+ * pragma solidity 0.4.24;
+ *
+ * contract ContractB {
+ *   event ContractBEvent(uint256 v);
+ *
+ *   function callEvent(uint256 _v) public {
+ *     emit ContractBEvent(_v);
+ *   }
+ * }
  *
  */
 
-const newContractAndClient = async () => {
-  const privKey = CryptoUtils.generatePrivateKey()
-  const client = createTestClient()
-  const from = LocalAddress.fromPublicKey(CryptoUtils.publicKeyFromPrivateKey(privKey)).toString()
-  const loomProvider = new LoomProvider(client, privKey)
-  const web3 = new Web3(loomProvider)
+function contractABIAndBinary() {
+  const contractBData =
+    '6080604052348015600f57600080fd5b5060db8061001e6000396000f300608060405260043610603f576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063f88d0dfc146044575b600080fd5b348015604f57600080fd5b50606c60048036038101908080359060200190929190505050606e565b005b806000819055507f8611c0f1e10aa104c81817ff1befe6e3677acee7991f16f99a8c375ca0793120816040518082815260200191505060405180910390a1505600a165627a7a723058203819249ad266695de9c923df5f4b3d2b244d3f6ba4297db60b92c4955bec2c230029'
 
-  const contractData =
-    '0x608060405234801561001057600080fd5b50600a60008190555061010e806100286000396000f3006080604052600436106049576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806360fe47b114604e5780636d4ce63c146078575b600080fd5b348015605957600080fd5b5060766004803603810190808035906020019092919050505060a0565b005b348015608357600080fd5b50608a60d9565b6040518082815260200191505060405180910390f35b806000819055506000547fb922f092a64f1a076de6f21e4d7c6400b6e55791cc935e7bb8e7e90f7652f15b60405160405180910390a250565b600080549050905600a165627a7a72305820b76f6c855a1f95260fc70490b16774074225da52ea165a58e95eb7a72a59d1700029'
+  const contractAData =
+    '608060405234801561001057600080fd5b50610181806100206000396000f300608060405260043610610041576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063a045846314610046575b600080fd5b34801561005257600080fd5b5061009160048036038101908080359060200190929190803573ffffffffffffffffffffffffffffffffffffffff169060200190929190505050610093565b005b7fbb6ecd3f0ef42d655786d77b262f49bee128d78f171832c1ea73b1383674c23a826040518082815260200191505060405180910390a18073ffffffffffffffffffffffffffffffffffffffff1663f88d0dfc836040518263ffffffff167c010000000000000000000000000000000000000000000000000000000002815260040180828152602001915050600060405180830381600087803b15801561013957600080fd5b505af115801561014d573d6000803e3d6000fd5b5050505050505600a165627a7a72305820101762f1d82f0bc7e12cf1b12098f2dfda892ad477dfd98760c2efab436ae3600029'
 
-  const ABI = [
+  const ABIContractB = [
     {
       constant: false,
-      inputs: [{ name: '_value', type: 'uint256' }],
-      name: 'set',
+      inputs: [{ name: '_v', type: 'uint256' }],
+      name: 'callEvent',
       outputs: [],
       payable: false,
       stateMutability: 'nonpayable',
       type: 'function'
     },
     {
-      constant: true,
-      inputs: [],
-      name: 'get',
-      outputs: [{ name: '', type: 'uint256' }],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    { inputs: [], payable: false, stateMutability: 'nonpayable', type: 'constructor' },
-    {
       anonymous: false,
-      inputs: [{ indexed: true, name: '_value', type: 'uint256' }],
-      name: 'NewValueSet',
+      inputs: [{ indexed: false, name: 'v', type: 'uint256' }],
+      name: 'ContractBEvent',
       type: 'event'
     }
   ]
 
-  const result = await deployContract(loomProvider, contractData)
+  const ABIContractA = [
+    {
+      constant: false,
+      inputs: [{ name: '_v', type: 'uint256' }, { name: '_contractBAddr', type: 'address' }],
+      name: 'doEmit',
+      outputs: [],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function'
+    },
+    {
+      anonymous: false,
+      inputs: [{ indexed: false, name: 'v', type: 'uint256' }],
+      name: 'ContractAEvent',
+      type: 'event'
+    }
+  ]
 
-  const contract = new web3.eth.Contract(ABI, result.contractAddress, { from })
-
-  return {
-    contract,
-    client
-  }
+  return { contractBData, contractAData, ABIContractB, ABIContractA }
 }
 
-test('LoomProvider + Web3 past events', async t => {
+async function testContracts(t: Test, contractB: any, contractA: any) {
   try {
-    const { contract, client } = await newContractAndClient()
-    const numEvents = 5
+    const value = 5
 
-    for (let v = 1; v <= numEvents; v++) {
-      const tx = await contract.methods.set(v).send()
-      t.equal(tx.status, true, `SimpleStore.set should return correct status for ${v}`)
+    let tx = await contractB.methods.callEvent(value).send()
+    t.equal(tx.status, true, `callEvent should return correct status for ${value}`)
 
-      const resultOfGet = await contract.methods.get().call()
-      t.equal(+resultOfGet, v, `SimpleStore.get should return correct value for ${v}`)
-    }
+    await waitForMillisecondsAsync(1000)
 
-    await waitForMillisecondsAsync(2000)
+    contractB.getPastEvents('ContractBEvent', (err: Error, events: any) => {
+      t.assert(!err)
+      const [event] = events
+      t.equal(+event.returnValues.v, value, `Should return value ${value}`)
+    })
 
-    contract.getPastEvents(
-      {
-        fromBlock: 0,
-        toBlock: 'latest'
-      },
-      (_err: Error, events: any) => {
-        t.assert(
-          events.every((event: any) => event.event === 'NewValueSet'),
-          `NewValueSet in ${numEvents} events`
-        )
-        t.equal(events.length, numEvents, `Should have ${numEvents} events registered`)
-      }
-    )
+    tx = await contractA.methods.doEmit(value, contractB.options.address).send()
+    t.equal(tx.status, true, `doEmit should return correct status for ${value}`)
 
-    await waitForMillisecondsAsync(2000)
+    await waitForMillisecondsAsync(1000)
 
-    client.disconnect()
+    contractA.getPastEvents('ContractAEvent', (err: Error, events: any) => {
+      t.assert(!err)
+      const [event] = events
+      t.equal(+event.returnValues.v, value, `Should return value ${value}`)
+    })
+
+    await waitForMillisecondsAsync(1000)
   } catch (err) {
     console.log(err)
   }
+}
 
+async function deployContractGanache(web3Provider: any, contractData: string) {
+  const web3 = new Web3(web3Provider)
+  const fromAddr = '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1'
+
+  const ethSendTransactionDeployResult = await web3.eth.sendTransaction({
+    from: fromAddr,
+    data: contractData,
+    gas: '300000',
+    gasPrice: '0x1'
+  })
+
+  if (!ethSendTransactionDeployResult.status) {
+    throw Error('Cant deploy contract on ganache')
+  }
+
+  const ethGetTransactionReceiptResult = await web3.eth.getTransactionReceipt(
+    ethSendTransactionDeployResult.transactionHash
+  )
+
+  return ethGetTransactionReceiptResult
+}
+
+async function testGanache(t: Test) {
+  const from = '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1'
+
+  const websocketProvider = new Web3.providers.WebsocketProvider('ws://127.0.0.1:8545')
+  const web3 = new Web3(websocketProvider)
+
+  const { contractBData, contractAData, ABIContractB, ABIContractA } = contractABIAndBinary()
+
+  const resultB = await deployContractGanache(websocketProvider, contractBData)
+  const resultA = await deployContractGanache(websocketProvider, contractAData)
+
+  const contractB = new web3.eth.Contract(ABIContractB, resultB.contractAddress, { from })
+  const contractA = new web3.eth.Contract(ABIContractA, resultA.contractAddress, { from })
+
+  t.comment('Testing Ganache')
+  await testContracts(t, contractB, contractA)
+
+  web3.currentProvider.connection.close()
+}
+
+async function testLoomProvider(t: Test) {
+  const privKey = CryptoUtils.generatePrivateKey()
+  const client = createTestClient()
+  const from = LocalAddress.fromPublicKey(CryptoUtils.publicKeyFromPrivateKey(privKey)).toString()
+  const loomProvider = new LoomProvider(client, privKey)
+  const web3 = new Web3(loomProvider)
+
+  const { contractBData, contractAData, ABIContractB, ABIContractA } = contractABIAndBinary()
+
+  const resultB = await deployContract(loomProvider, contractBData)
+  const resultA = await deployContract(loomProvider, contractAData)
+
+  const contractB = new web3.eth.Contract(ABIContractB, resultB.contractAddress, { from })
+  const contractA = new web3.eth.Contract(ABIContractA, resultA.contractAddress, { from })
+
+  t.comment('Testing Loom Provider')
+  await testContracts(t, contractB, contractA)
+
+  client.disconnect()
+}
+
+test('Test Web3 + PastEvents', async t => {
+  await testGanache(t)
+  await testLoomProvider(t)
   t.end()
 })
