d21ec3cfea80cd37380a77299daaf0718c612eba
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2018-09-03 08:10:36 -0300

    Fixed return type

--- src/contracts/eth-coin.ts
+++ src/contracts/eth-coin.ts
@@ -3,12 +3,16 @@ import { Client } from '../client'
 import { Address } from '../address'
 
 export class EthCoin extends Coin {
-  static async createAsync(client: Client, callerAddr: Address): Promise<Coin> {
+  static async createAsync(client: Client, callerAddr: Address): Promise<EthCoin> {
     const contractAddr = await client.getContractAddressAsync('ethcoin')
     if (!contractAddr) {
       throw Error('Failed to resolve contract address')
     }
 
-    return new Coin({ contractAddr, callerAddr, client })
+    return new EthCoin({ contractAddr, callerAddr, client })
+  }
+
+  constructor(params: { contractAddr: Address; callerAddr: Address; client: Client }) {
+    super(params)
   }
 }
