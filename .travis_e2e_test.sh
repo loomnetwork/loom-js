f78f39ec31d54888616bb5927f4a9800108d9214
Vadim Macagon <vadim.macagon@gmail.com>
2018-09-21 17:52:33 +0700

    Refactor Travis e2e script to make it easier to run locally
    Can now set LOOM_BIN and LOOM_BLUEPRINT_DIR to run the tests with
    prebuilt versions of loom & the blueprint contract.
    
    Also tweaked the script to ensure the blueprint contract process is
    killed on exit.

--- .travis_e2e_test.sh
+++ .travis_e2e_test.sh
@@ -2,63 +2,6 @@
 
 set -euxo pipefail
 
-# Prepare env
-BUILD_NUMBER=432
-GANACHE_PORT=8545
+eval "$(GIMME_GO_VERSION=1.10.2 gimme)"
 
-# Setup and spins up a Ganache node
-setup_ganache() {
-  yarn add ganache-cli
-  ./node_modules/.bin/ganache-cli -d -p $GANACHE_PORT &
-  ganache_pid=$!
-}
-
-setup_go() {
-  eval "$(GIMME_GO_VERSION=1.10.2 gimme)"
-}
-
-setup_go_loom() {
-  go get github.com/loomnetwork/go-loom
-}
-
-setup_dappchain() {
-  wget https://private.delegatecall.com/loom/linux/build-$BUILD_NUMBER/loom
-  chmod +x loom
-  loom_bin=`pwd`/loom
-}
-
-setup_weave_blueprint() {
-  git clone https://github.com/loomnetwork/weave-blueprint.git
-  cd weave-blueprint
-  export GOPATH=$GOPATH:`pwd`
-  make deps
-  make
-  cd build
-  $loom_bin init -f
-  cp -R ./../../e2e_support/* .
-  $loom_bin run &
-  loom_pid=$!
-  sleep 5
-  $loom_bin deploy -a public_key_acc_1 -k private_key_acc_1 -n SimpleStore -b ./SimpleStore.bin
-  cd ../..
-}
-
-run_test() {
-  yarn test:node
-  yarn test:browser
-  yarn e2e:node
-}
-
-cleanup() {
-  kill -9 $ganache_pid
-  kill -9 $loom_pid
-}
-
-trap cleanup EXIT
-
-setup_ganache
-setup_go
-setup_go_loom
-setup_dappchain
-setup_weave_blueprint
-run_test
+bash e2e_tests.sh
---
+++ e2e_tests.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# To run this script with a locally built loom binary set the LOOM_BIN env var to point to your loom
+# binary. If you plan on running the tests frequently you'll save a lot of time if you run the
+# steps in the setup_weave_blueprint in another directory, and then set LOOM_BLUEPRINT_DIR env var
+# to point to it, otherwise the contract will be recloned and rebuilt every time.
+
+set -euxo pipefail
+
+# Prepare env
+DEFAULT_GOPATH=$GOPATH
+BUILD_NUMBER=432
+GANACHE_PORT=8545
+REPO_ROOT=`pwd`
+LOOM_DIR=`pwd`/tmp/e2e
+
+download_dappchain() {
+  cd $LOOM_DIR
+  wget https://private.delegatecall.com/loom/linux/build-$BUILD_NUMBER/loom
+  chmod +x loom
+  LOOM_BIN=`pwd`/loom
+}
+
+setup_weave_blueprint() {
+  cd $LOOM_DIR
+  git clone https://github.com/loomnetwork/weave-blueprint.git
+  cd weave-blueprint
+  LOOM_BLUEPRINT_DIR=`pwd`
+  make deps
+  make
+}
+
+setup_dappchain() {
+  cd $LOOM_DIR
+  $LOOM_BIN init -f
+  cp -R $REPO_ROOT/e2e_support/* .
+  mkdir -p contracts
+  cp $LOOM_BLUEPRINT_DIR/build/contracts/* contracts
+}
+
+start_chains() {
+  $REPO_ROOT/node_modules/.bin/ganache-cli -d -p $GANACHE_PORT &
+  GANACHE_PID=$!
+
+  cd $LOOM_DIR
+  $LOOM_BIN run &
+  LOOM_PID=$!
+  sleep 5
+
+  $LOOM_BIN deploy -a public_key_acc_1 -k private_key_acc_1 -n SimpleStore -b ./SimpleStore.bin
+}
+
+stop_chains() {
+  kill -9 $GANACHE_PID
+  kill -9 $LOOM_PID
+  pkill -f "${LOOM_DIR}/contracts/blueprint.0.0.1" || true
+}
+
+run_tests() {
+  yarn test:node
+  yarn test:browser
+  yarn e2e:node
+}
+
+cleanup() {
+  stop_chains
+  export GOPATH=$DEFAULT_GOPATH
+}
+
+rm -rf $LOOM_DIR; true
+mkdir -p $LOOM_DIR
+
+if [[ -z "${LOOM_BLUEPRINT_DIR:-}" ]]; then
+  setup_weave_blueprint
+fi
+
+setup_dappchain
+
+if [[ -z "${LOOM_BIN:-}" ]]; then
+  download_dappchain
+fi
+
+trap cleanup EXIT
+
+start_chains
+run_tests
