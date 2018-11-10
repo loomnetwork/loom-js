68cd32c19059bb70d6137395df211f49b95fa6ed
Georgios Konstantopoulos <me@gakonst.com>
2018-11-10 23:37:57 +0200

    Remove signed contract Update web3Signer Remove OfflineWeb3Signer

--- src/solidity-helpers.ts
+++ src/solidity-helpers.ts
@@ -1,6 +1,6 @@
 import ethutil from 'ethereumjs-util'
 import Web3 from 'web3'
-import { Account } from 'web3/eth/accounts'
+import { ethers } from 'ethers'
 
 const web3 = new Web3()
 
@@ -20,16 +20,14 @@ export interface IEthereumSigner {
  * This signer should be used for interactive signing in the browser with MetaMask.
  */
 export class Web3Signer implements IEthereumSigner {
-  private _web3: Web3
-  private _address: string
+  private _wallet: ethers.Signer
 
   /**
    * @param web3 Web3 instance to use for signing.
    * @param accountAddress Address of web3 account to sign with.
    */
-  constructor(web3: Web3, accountAddress: string) {
-    this._web3 = web3
-    this._address = accountAddress
+  constructor(wallet: ethers.Signer) {
+    this._wallet = wallet
   }
 
   /**
@@ -38,55 +36,8 @@ export class Web3Signer implements IEthereumSigner {
    * @returns Promise that will be resolved with the signature bytes.
    */
   async signAsync(msg: string): Promise<Uint8Array> {
-    const signature = await this._web3.eth.sign(msg, this._address)
-    const sig = signature.slice(2)
-
-    let mode = 1 // Geth
-    const r = ethutil.toBuffer('0x' + sig.substring(0, 64)) as Buffer
-    const s = ethutil.toBuffer('0x' + sig.substring(64, 128)) as Buffer
-    let v = parseInt(sig.substring(128, 130), 16)
-
-    if (v === 0 || v === 1) {
-      v += 27
-    } else {
-      mode = 0 // indicate that msg wasn't prefixed before signing (MetaMask doesn't prefix!)
-    }
-    return Buffer.concat([ethutil.toBuffer(mode) as Buffer, r, s, ethutil.toBuffer(v) as Buffer])
-  }
-}
-
-/**
- * Signs message using a Web3 account.
- * This signer should be used for signing in NodeJS.
- */
-export class OfflineWeb3Signer implements IEthereumSigner {
-  private _web3: Web3
-  private _account: Account
-
-  /**
-   * @param web3 Web3 instance to use for signing.
-   * @param accountAddress Address of web3 account to sign with.
-   */
-  constructor(web3: Web3, account: Account) {
-    this._web3 = web3
-    this._account = account
-  }
-
-  /**
-   * Signs a message.
-   * @param msg Message to sign.
-   * @returns Promise that will be resolved with the signature bytes.
-   */
-  async signAsync(msg: string): Promise<Uint8Array> {
-    const ret = await this._web3.eth.accounts.sign(msg, this._account.privateKey)
-    // @ts-ignore
-    const sig = ret.signature.slice(2)
-
-    let mode = 1 // Geth
-    const r = ethutil.toBuffer('0x' + sig.substring(0, 64)) as Buffer
-    const s = ethutil.toBuffer('0x' + sig.substring(64, 128)) as Buffer
-    let v = parseInt(sig.substring(128, 130), 16)
-
-    return Buffer.concat([ethutil.toBuffer(mode) as Buffer, r, s, ethutil.toBuffer(v) as Buffer])
+    const flatSig = await this._wallet.signMessage(ethers.utils.arrayify(msg))
+    const sig = '0x01' + flatSig.slice(2) // prepend 0x01 mode for Ethereum Signed Message
+    return ethutil.toBuffer(sig)
   }
 }
