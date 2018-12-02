9c59c23079fd71035a2e2823b55237bd287390eb
Georgios Konstantopoulos <me@gakonst.com>
2018-12-02 15:52:08 +0200

    Fix api / variable naming

--- src/contracts/transfer-gateway.ts
+++ src/contracts/transfer-gateway.ts
@@ -263,17 +263,17 @@ export class TransferGateway extends Contract {
    * Sends a request to the DAppChain Gateway to begin withdrawal of LOOM from the current
    * DAppChain account to an Ethereum account.
    * @param amount Amount to withdraw.
-   * @param ethereumGateway Ethereum address of Ethereum Gateway.
+   * @param mainnetLoomcoinAddress Ethereum address of Ethereum Gateway.
    * @param recipient Ethereum address of the account the token should be withdrawn to, if this is
    *                  omitted the Gateway will attempt to use the Address Mapper to retrieve the
    *                  address of the Ethereum account mapped to the current DAppChain account.
    * @returns A promise that will be resolved when the DAppChain Gateway has accepted the withdrawal
    *          request.
    */
-  withdrawLoomCoinAsync(amount: BN, ethereumGateway: Address, recipient?: Address): Promise<void> {
+  withdrawLoomCoinAsync(amount: BN, mainnetLoomcoinAddress: Address, recipient?: Address): Promise<void> {
     const req = new TransferGatewayWithdrawLoomCoinRequest()
     req.setAmount(marshalBigUIntPB(amount))
-    req.setMainnetLoomcoinGateway(ethereumGateway.MarshalPB())
+    req.setTokenContract(mainnetLoomcoinAddress.MarshalPB())
     if (recipient) {
       req.setRecipient(recipient.MarshalPB())
     }
