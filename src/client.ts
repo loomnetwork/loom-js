1b384f9d0c1f6eadc2b940eaab3024834c8ba2a6
Vadim Macagon <vadim.macagon@gmail.com>
2018-12-21 22:39:59 +1300

    Try using TxCommitBroadcaster to see if failing tests pass

--- src/client.ts
+++ src/client.ts
@@ -263,7 +263,7 @@ export class Client extends EventEmitter {
   ) {
     super()
     this.chainId = chainId
-    this.txBroadcaster = new TxSyncBroadcaster()
+    this.txBroadcaster = new TxCommitBroadcaster()
 
     // TODO: basic validation of the URIs to ensure they have all required components.
     this._writeClient =
