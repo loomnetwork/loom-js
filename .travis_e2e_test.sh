92aaf0f354a9a4dd061ed719a1b9e0c5a29b79fd
Vadim Macagon <vadim.macagon@gmail.com>
2019-01-07 14:10:31 +0700

    Run tests against latest loom build

--- .travis_e2e_test.sh
+++ .travis_e2e_test.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 eval "$(GIMME_GO_VERSION=1.10.2 gimme)"
 
-export BUILD_ID=nonce-on-write-url
+export BUILD_ID=build-726
 
 bash e2e_tests.sh
-#bash e2e_plasma_cash_test.sh
+bash e2e_plasma_cash_test.sh
