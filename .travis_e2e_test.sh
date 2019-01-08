0b27f4341fbf2a2dee572bd42b7d34ff6fb78182
Eduardo Pereira <eduardonunesp@gmail.com>
2019-01-08 11:36:33 -0200

    Test plasma if PLASMA_TEST available

--- .travis_e2e_test.sh
+++ .travis_e2e_test.sh
@@ -7,4 +7,7 @@ eval "$(GIMME_GO_VERSION=1.10.2 gimme)"
 export BUILD_ID=build-726
 
 bash e2e_tests.sh
-bash e2e_plasma_cash_test.sh
+
+if [ "${PLASMA_TEST:-}" ]; then
+  bash e2e_plasma_cash_test.sh
+fi
