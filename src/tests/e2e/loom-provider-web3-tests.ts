15c3336c11883535f02a7c7167ff79625b44cc06
Eduardo Pereira <eduardonunesp@gmail.com>
2019-01-09 18:37:05 -0200

    Testing possible issue

--- src/tests/e2e/loom-provider-web3-tests.ts
+++ src/tests/e2e/loom-provider-web3-tests.ts
@@ -21,6 +21,7 @@ const Web3 = require('web3')
  *
  * contract SimpleStore {
  *   uint value;
+ *
  *   constructor() public {
  *       value = 10;
  *   }
@@ -30,6 +31,12 @@ const Web3 = require('web3')
  *   function set(uint _value) public {
  *     value = _value;
  *     emit NewValueSet(value);
+ *   }
+ *
+ *   function setTwiceEvent(uint _value) public {
+ *     value = _value;
+ *     emit NewValueSet(_value);
+ *     emit NewValueSet(_value + 1);
  *   }
  *
  *   function get() public view returns (uint) {
@@ -50,12 +57,31 @@ const newContractAndClient = async () => {
   client.on('error', console.log)
 
   const contractData =
-    '0x608060405234801561001057600080fd5b50600a60008190555061010e806100286000396000f3006080604052600436106049576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806360fe47b114604e5780636d4ce63c146078575b600080fd5b348015605957600080fd5b5060766004803603810190808035906020019092919050505060a0565b005b348015608357600080fd5b50608a60d9565b6040518082815260200191505060405180910390f35b806000819055506000547fb922f092a64f1a076de6f21e4d7c6400b6e55791cc935e7bb8e7e90f7652f15b60405160405180910390a250565b600080549050905600a165627a7a72305820b76f6c855a1f95260fc70490b16774074225da52ea165a58e95eb7a72a59d1700029'
+    '608060405234801561001057600080fd5b50600a6000819055506101b6806100286000396000f300608060405260043610610057576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063076a66cc1461005c57806360fe47b1146100895780636d4ce63c146100b6575b600080fd5b34801561006857600080fd5b50610087600480360381019080803590602001909291905050506100e1565b005b34801561009557600080fd5b506100b460048036038101908080359060200190929190505050610148565b005b3480156100c257600080fd5b506100cb610181565b6040518082815260200191505060405180910390f35b80600081905550807fb922f092a64f1a076de6f21e4d7c6400b6e55791cc935e7bb8e7e90f7652f15b60405160405180910390a2600181017fb922f092a64f1a076de6f21e4d7c6400b6e55791cc935e7bb8e7e90f7652f15b60405160405180910390a250565b806000819055506000547fb922f092a64f1a076de6f21e4d7c6400b6e55791cc935e7bb8e7e90f7652f15b60405160405180910390a250565b600080549050905600a165627a7a72305820dc62f81ecfe4f7410f8580dbb11d9d0af6ac80172a722d579e5a6a5344d742060029'
 
   const ABI = [
     {
       constant: false,
-      inputs: [{ name: '_value', type: 'uint256' }],
+      inputs: [
+        {
+          name: '_value',
+          type: 'uint256'
+        }
+      ],
+      name: 'setTwiceEvent',
+      outputs: [],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [
+        {
+          name: '_value',
+          type: 'uint256'
+        }
+      ],
       name: 'set',
       outputs: [],
       payable: false,
@@ -66,15 +92,31 @@ const newContractAndClient = async () => {
       constant: true,
       inputs: [],
       name: 'get',
-      outputs: [{ name: '', type: 'uint256' }],
+      outputs: [
+        {
+          name: '',
+          type: 'uint256'
+        }
+      ],
       payable: false,
       stateMutability: 'view',
       type: 'function'
     },
-    { inputs: [], payable: false, stateMutability: 'nonpayable', type: 'constructor' },
+    {
+      inputs: [],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'constructor'
+    },
     {
       anonymous: false,
-      inputs: [{ indexed: true, name: '_value', type: 'uint256' }],
+      inputs: [
+        {
+          indexed: true,
+          name: '_value',
+          type: 'uint256'
+        }
+      ],
       name: 'NewValueSet',
       type: 'event'
     }
@@ -207,6 +249,62 @@ test('LoomProvider getBlockByNumber', async t => {
     const blockNumber = await web3.eth.getBlockNumber()
     const blockInfo = await web3.eth.getBlock(blockNumber, false)
     t.equal(parseInt(blockInfo.blockNumber, 16), blockNumber, 'Block number should be equal')
+  } catch (err) {
+    console.log(err)
+  }
+
+  if (client) {
+    client.disconnect()
+  }
+
+  t.end()
+})
+
+test.only('LoomProvider + Web3 + Dispatch event twice same contract and function', async t => {
+  // We're planning to execute 4 asserts
+  t.plan(4)
+
+  const { contract, client } = await newContractAndClient()
+  try {
+    const newValue = 1
+    const eventValues = new Array<number>()
+    contract.events.NewValueSet({}, (err: Error, event: any) => {
+      if (err) t.error(err)
+      else {
+        // Assert 1
+        t.assert(
+          +event.returnValues._value >= 1,
+          `Value should be returned ${event.returnValues._value}`
+        )
+
+        eventValues.push(+event.returnValues._value)
+      }
+    })
+
+    contract.events.NewValueSet({}, (err: Error, event: any) => {
+      if (err) t.error(err)
+      else {
+        // Assert 2
+        t.assert(
+          +event.returnValues._value >= 1,
+          `Value should be returned ${event.returnValues._value}`
+        )
+
+        eventValues.push(+event.returnValues._value)
+      }
+    })
+
+    const receipt = await contract.methods.setTwiceEvent(newValue).send()
+
+    // Assert 3
+    t.equal(receipt.status, '0x1', 'SimpleStore.set should return correct status')
+
+    await waitForMillisecondsAsync(2000)
+
+    const eventSum = eventValues.reduce((acc, currValue) => acc + currValue)
+
+    // Assert 4
+    t.equal(eventSum, 3, `Sum of two events should be ${eventSum}`)
   } catch (err) {
     console.log(err)
   }
