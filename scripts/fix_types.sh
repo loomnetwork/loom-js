133df3b48be1acaed78c3d25c78d0a25c3ad383b
Eduardo Pereira <eduardonunesp@gmail.com>
2019-02-23 16:35:07 -0300

    Fix shell script

--- scripts/fix_types.sh
+++ scripts/fix_types.sh
@@ -1,8 +1,10 @@
-unamestr=(uname)
+#!/bin/bash
+
+unamestr=$(uname)
 cmd="sed"
 if [[ "$unamestr" == 'Darwin' ]]; then
   cmd="gsed"
 fi
 
 # Typechain bug https://github.com/ethereum-ts/TypeChain/issues/98
-gsed -i "s/sig: (string)\[\],/sig: string,/g" src/mainnet-contracts/ERC20Gateway.d.ts
+$cmd -i "s/sig: (string)\[\],/sig: string,/g" src/mainnet-contracts/ERC20Gateway.d.ts
