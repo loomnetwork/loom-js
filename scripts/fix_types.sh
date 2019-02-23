4e784eb1e12d8a3b1ab30b613c3c9a20887d8d1c
Eduardo Pereira <eduardonunesp@gmail.com>
2019-02-23 16:29:42 -0300

    Upldate script and yarn lock

--- scripts/fix_types.sh
+++ scripts/fix_types.sh
@@ -1,2 +1,8 @@
+unamestr=(uname)
+cmd="sed"
+if [[ "$unamestr" == 'Darwin' ]]; then
+  cmd="gsed"
+fi
+
 # Typechain bug https://github.com/ethereum-ts/TypeChain/issues/98
-sed -i "s/sig: (string)\[\],/sig: string,/g" src/mainnet-contracts/ERC20Gateway.d.ts
+gsed -i "s/sig: (string)\[\],/sig: string,/g" src/mainnet-contracts/ERC20Gateway.d.ts
