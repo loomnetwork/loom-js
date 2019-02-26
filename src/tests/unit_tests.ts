91df807ba3161214383e07fe6bb83f5d7b26fcf3
Vadim Macagon <vadim.macagon@gmail.com>
2019-02-27 02:19:57 +0700

    Disable more tests

--- src/tests/unit_tests.ts
+++ src/tests/unit_tests.ts
@@ -1,6 +1,6 @@
-/*
 import './unit/address-tests'
 import './unit/big-uint-tests'
+/*
 import './unit/client-url-override-tests'
 import './unit/rpc-client-factory-tests'
 import './unit/sparse-merkle-tree-tests'
