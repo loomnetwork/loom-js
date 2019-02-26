36cbd3e7894ed9df9dd659656ffd8d075f10ae55
Vadim Macagon <vadim.macagon@gmail.com>
2019-02-27 01:53:53 +0700

    More logging

--- src/middleware/cached-nonce-tx-middleware.ts
+++ src/middleware/cached-nonce-tx-middleware.ts
@@ -25,16 +25,16 @@ export class CachedNonceTxMiddleware implements ITxMiddlewareHandler {
 
   async Handle(txData: Readonly<Uint8Array>): Promise<Uint8Array> {
     if (this._lastNonce === null) {
-      console.log('Nonce not found getting from loomchain')
+      console.log('CachedNonceTxMiddleware Nonce not found getting from loomchain')
       try {
         const key = bytesToHex(this._publicKey)
         this._lastNonce = await this._client.getNonceAsync(key)
       } catch (err) {
-        throw Error('Failed to obtain latest nonce')
+        throw Error('CachedNonceTxMiddleware Failed to obtain latest nonce')
       }
     }
 
-    console.log(`Next nonce ${this._lastNonce + 1}`)
+    console.log(`CachedNonceTxMiddleware Next nonce ${this._lastNonce + 1}`)
 
     const tx = new NonceTx()
     tx.setInner(txData as Uint8Array)
@@ -75,7 +75,7 @@ export class CachedNonceTxMiddleware implements ITxMiddlewareHandler {
       // which means the cached nonce has diverged from the nonce on the node, need to clear it out
       // so it's refetched for the next tx.
       this._lastNonce = null
-      console.log('Reset cached nonce')
+      console.log('CachedNonceTxMiddleware Reset cached nonce')
     }
   }
 }
--- src/tests/e2e/client-test-tx-cache.ts
+++ src/tests/e2e/client-test-tx-cache.ts
@@ -296,6 +296,7 @@ test('Test CachedNonceTxMiddleware - duplicate tx', async t => {
 
     try {
       // Should not fail
+      console.log("Sending tx #1")
       await callTransactionAsync(client, caller, address, functionSetOk)
     } catch (err) {
       console.error(err)
@@ -304,6 +305,7 @@ test('Test CachedNonceTxMiddleware - duplicate tx', async t => {
 
     try {
       // Should not fail, and should force the nonce to be incremented on the node
+      console.log("Sending tx #2")
       await callTransactionAsync(client2, caller, address, functionSetOk)
     } catch (err) {
       console.error(err)
@@ -312,6 +314,7 @@ test('Test CachedNonceTxMiddleware - duplicate tx', async t => {
 
     try {
       // Should fail because cached nonce doesn't match the one on the node anymore
+      console.log("Sending tx #3")
       await callTransactionAsync(client, caller, address, functionSetOk)
     } catch (err) {
       console.error(err)
@@ -321,6 +324,7 @@ test('Test CachedNonceTxMiddleware - duplicate tx', async t => {
     try {
       // Should not fail because the cached nonce should've been reset, and a fresh nonce should
       // be used for this call
+      console.log("Sending tx #4")
       await callTransactionAsync(client, caller, address, functionSetOk)
     } catch (err) {
       console.error(err)
