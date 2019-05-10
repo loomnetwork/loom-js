051468a21de95d1c2f752a68ef50d50e84a9aeb4
Deeprom <grooveeeeee@gmail.com>
2019-05-10 12:11:23 +0700

    yarn format

--- src/types/tronweb.d.ts
+++ src/types/tronweb.d.ts
@@ -1,17 +1,15 @@
 declare module 'tronweb' {
-
   export class TronWeb {
-    sha3(msg: string);
-    constructor(e: any, r: any, ...args: any[]);
-    setAddress(publicAddress: string);
-    setPrivateKey(privatekey: string);
-    getEventResult(contractAddress: string, contractName: string);
+    sha3(msg: string)
+    constructor(e: any, r: any, ...args: any[])
+    setAddress(publicAddress: string)
+    setPrivateKey(privatekey: string)
+    getEventResult(contractAddress: string, contractName: string)
     trx: {
-      sign(transaction: string, ...args: any[]);
-      signMessage(...args: any[]);
+      sign(transaction: string, ...args: any[])
+      signMessage(...args: any[])
     }
-
   }
 
-  export default TronWeb;
+  export default TronWeb
 }
