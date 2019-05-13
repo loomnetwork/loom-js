b514a967875c986b4070aca06243fde92da300b8
Deeprom <grooveeeeee@gmail.com>
2019-05-13 12:10:12 +0700

    move sha3 below constructor

--- src/types/tronweb.d.ts
+++ src/types/tronweb.d.ts
@@ -1,10 +1,10 @@
 declare module 'tronweb' {
   export class TronWeb {
-    sha3(msg: string)
     constructor(e: any, r: any, ...args: any[])
+    getEventResult(contractAddress: string, contractName: string)
     setAddress(publicAddress: string)
     setPrivateKey(privatekey: string)
-    getEventResult(contractAddress: string, contractName: string)
+    sha3(msg: string)
     trx: {
       sign(transaction: string, ...args: any[])
       signMessage(...args: any[])
