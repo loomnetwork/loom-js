7353a65a2cb864141f59f3063d1ab5a809e673b9
mukuls9971 <mukul9971@gmail.com>
2019-05-22 16:47:23 +0530

    resolved some bugs

--- src/contracts/user_deployer_whitelist.ts
+++ src/contracts/user_deployer_whitelist.ts
@@ -11,41 +11,37 @@ import {
 } from '../proto/user_deployer_whitelist_pb'
 
 import {Address as LoomAddress} from '../proto/loom_pb'
+import {Deployer} from '../proto/deployer_whitelist_pb'
 
 export class UserDeployerWhitelist extends Contract {
-  static async createAsync(client: Client, callerAddr: Address): Promise < UserDeployerWhitelist > {
-    const contractAddr = await client.getContractAddressAsync('user_deployer_whitelist')
-    if (!contractAddr) {
-      throw Error('Failed to resolve contract address')
-    }
+         static async createAsync(client: Client, callerAddr: Address): Promise<UserDeployerWhitelist> {
+           const contractAddr = await client.getContractAddressAsync('user_deployer_whitelist')
+           if (!contractAddr) {
+             throw Error('Failed to resolve contract address')
+           }
 
-    return new UserDeployerWhitelist({
-      contractAddr,
-      callerAddr,
-      client
-    })
-  }
+           return new UserDeployerWhitelist({ contractAddr, callerAddr, client })
+         }
 
-  constructor(params: {contractAddr: Address; callerAddr: Address; client: Client}) {
-    super(params)
-  }
+         constructor(params: { contractAddr: Address; callerAddr: Address; client: Client }) {
+           super(params)
+         }
 
-  addUserDeployerAsync(deployerAddr: Address, tierID: TierID): Promise<void> {
-    const whitelistUserDeployerReq = new WhitelistUserDeployerRequest()
-    whitelistUserDeployerReq.setDeployeraddr(deployerAddr.MarshalPB())
-    whitelistUserDeployerReq.setTierId(tierID)
-    return this.callAsync<void>('AddUserDeployer', whitelistUserDeployerReq)
-  }
-  async getUserDeployersAsync(): Promise<Array<Address>> {
-    const getUserDeployerReq = new GetUserDeployersRequest()
-    const result = await this.staticCallAsync('GetUserDeployers', getUserDeployerReq, new GetUserDeployersResponse())
-    return result.getDeployersList() as Array<Address>
-  }
-  async getDeployedContractsAsync(deployerAddr: Address): Promise<Array<LoomAddress> > {
-    const getDeployedContractsReq = new GetDeployedContractsRequest()
-    getDeployedContractsReq.setDeployeraddr(deployerAddr.MarshalPB())
-    const result = await this.staticCallAsync('GetDeployedContracts', getDeployedContractsReq, new GetDeployedContractsResponse())
-    return result.getContractaddressesList() as Array<LoomAddress>
-  }
-
-}
+         addUserDeployerAsync(deployerAddr: Address, tierID: TierID): Promise<void> {
+           const whitelistUserDeployerReq = new WhitelistUserDeployerRequest()
+           whitelistUserDeployerReq.setDeployeraddr(deployerAddr.MarshalPB())
+           whitelistUserDeployerReq.setTierId(tierID)
+           return this.callAsync<void>('AddUserDeployer', whitelistUserDeployerReq)
+         }
+         async getUserDeployersAsync(): Promise<Array<Deployer>> {
+           const getUserDeployerReq = new GetUserDeployersRequest()
+           const result = await this.staticCallAsync('GetUserDeployers', getUserDeployerReq, new GetUserDeployersResponse())
+           return result.getDeployersList() as Array<Deployer>
+         }
+         async getDeployedContractsAsync(deployerAddr: Address): Promise<Array<LoomAddress>> {
+           const getDeployedContractsReq = new GetDeployedContractsRequest()
+           getDeployedContractsReq.setDeployeraddr(deployerAddr.MarshalPB())
+           const result = await this.staticCallAsync('GetDeployedContracts', getDeployedContractsReq, new GetDeployedContractsResponse())
+           return result.getContractaddressesList() as Array<LoomAddress>
+         }
+       }
