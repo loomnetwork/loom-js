8de73bf9a7a148b29b85586fa4677fbf2face9c7
suhasagg <suhasagg@gmail.com>
2019-05-25 09:40:12 +0530

    Change fees data type to BigUInt

--- src/proto/user_deployer_whitelist_pb.d.ts
+++ src/proto/user_deployer_whitelist_pb.d.ts
@@ -1,67 +1,53 @@
 // package: 
 // file: proto/user_deployer_whitelist.proto
 
-import * as jspb from 'google-protobuf'
-import * as proto_loom_pb from '../proto/loom_pb'
-import * as proto_deployer_whitelist_deployer_whitelist_pb from '../proto/deployer_whitelist/deployer_whitelist_pb'
+import * as jspb from "google-protobuf";
+import * as proto_loom_pb from "../proto/loom_pb";
+import * as proto_deployer_whitelist_deployer_whitelist_pb from "../proto/deployer_whitelist/deployer_whitelist_pb";
 
 export class Tier extends jspb.Message {
   getId(): TierID;
-
   setId(value: TierID): void;
 
-  getFee(): number;
-
-  setFee(value: number): void;
+  hasFee(): boolean;
+  clearFee(): void;
+  getFee(): proto_loom_pb.BigUInt | undefined;
+  setFee(value?: proto_loom_pb.BigUInt): void;
 
   getName(): string;
-
   setName(value: string): void;
 
   serializeBinary(): Uint8Array;
-
   toObject(includeInstance?: boolean): Tier.AsObject;
   static toObject(includeInstance: boolean, msg: Tier): Tier.AsObject;
-
-  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-  static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
   static serializeBinaryToWriter(message: Tier, writer: jspb.BinaryWriter): void;
-
   static deserializeBinary(bytes: Uint8Array): Tier;
-
   static deserializeBinaryFromReader(message: Tier, reader: jspb.BinaryReader): Tier;
 }
 
 export namespace Tier {
   export type AsObject = {
     id: TierID,
-    fee: number,
+    fee?: proto_loom_pb.BigUInt.AsObject,
     name: string,
   }
 }
 
 export class TierInfo extends jspb.Message {
   clearTiersList(): void;
-
   getTiersList(): Array<Tier>;
-
   setTiersList(value: Array<Tier>): void;
-
   addTiers(value?: Tier, index?: number): Tier;
 
   serializeBinary(): Uint8Array;
-
   toObject(includeInstance?: boolean): TierInfo.AsObject;
   static toObject(includeInstance: boolean, msg: TierInfo): TierInfo.AsObject;
-
-  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-  static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
   static serializeBinaryToWriter(message: TierInfo, writer: jspb.BinaryWriter): void;
-
   static deserializeBinary(bytes: Uint8Array): TierInfo;
-
   static deserializeBinaryFromReader(message: TierInfo, reader: jspb.BinaryReader): TierInfo;
 }
 
@@ -73,33 +59,22 @@ export namespace TierInfo {
 
 export class InitRequest extends jspb.Message {
   hasOwner(): boolean;
-
   clearOwner(): void;
-
   getOwner(): proto_loom_pb.Address | undefined;
-
   setOwner(value?: proto_loom_pb.Address): void;
 
   hasTierInfo(): boolean;
-
   clearTierInfo(): void;
-
   getTierInfo(): TierInfo | undefined;
-
   setTierInfo(value?: TierInfo): void;
 
   serializeBinary(): Uint8Array;
-
   toObject(includeInstance?: boolean): InitRequest.AsObject;
   static toObject(includeInstance: boolean, msg: InitRequest): InitRequest.AsObject;
-
-  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-  static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
   static serializeBinaryToWriter(message: InitRequest, writer: jspb.BinaryWriter): void;
-
   static deserializeBinary(bytes: Uint8Array): InitRequest;
-
   static deserializeBinaryFromReader(message: InitRequest, reader: jspb.BinaryReader): InitRequest;
 }
 
@@ -112,25 +87,17 @@ export namespace InitRequest {
 
 export class DeployerContract extends jspb.Message {
   hasContractaddress(): boolean;
-
   clearContractaddress(): void;
-
   getContractaddress(): proto_loom_pb.Address | undefined;
-
   setContractaddress(value?: proto_loom_pb.Address): void;
 
   serializeBinary(): Uint8Array;
-
   toObject(includeInstance?: boolean): DeployerContract.AsObject;
   static toObject(includeInstance: boolean, msg: DeployerContract): DeployerContract.AsObject;
-
-  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-  static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
   static serializeBinaryToWriter(message: DeployerContract, writer: jspb.BinaryWriter): void;
-
   static deserializeBinary(bytes: Uint8Array): DeployerContract;
-
   static deserializeBinaryFromReader(message: DeployerContract, reader: jspb.BinaryReader): DeployerContract;
 }
 
@@ -142,29 +109,20 @@ export namespace DeployerContract {
 
 export class WhitelistUserDeployerRequest extends jspb.Message {
   hasDeployeraddr(): boolean;
-
   clearDeployeraddr(): void;
-
   getDeployeraddr(): proto_loom_pb.Address | undefined;
-
   setDeployeraddr(value?: proto_loom_pb.Address): void;
 
   getTierId(): TierID;
-
   setTierId(value: TierID): void;
 
   serializeBinary(): Uint8Array;
-
   toObject(includeInstance?: boolean): WhitelistUserDeployerRequest.AsObject;
   static toObject(includeInstance: boolean, msg: WhitelistUserDeployerRequest): WhitelistUserDeployerRequest.AsObject;
-
-  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-  static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
   static serializeBinaryToWriter(message: WhitelistUserDeployerRequest, writer: jspb.BinaryWriter): void;
-
   static deserializeBinary(bytes: Uint8Array): WhitelistUserDeployerRequest;
-
   static deserializeBinaryFromReader(message: WhitelistUserDeployerRequest, reader: jspb.BinaryReader): WhitelistUserDeployerRequest;
 }
 
@@ -177,33 +135,22 @@ export namespace WhitelistUserDeployerRequest {
 
 export class UserState extends jspb.Message {
   hasUseraddr(): boolean;
-
   clearUseraddr(): void;
-
   getUseraddr(): proto_loom_pb.Address | undefined;
-
   setUseraddr(value?: proto_loom_pb.Address): void;
 
   clearDeployersList(): void;
-
   getDeployersList(): Array<proto_loom_pb.Address>;
-
   setDeployersList(value: Array<proto_loom_pb.Address>): void;
-
   addDeployers(value?: proto_loom_pb.Address, index?: number): proto_loom_pb.Address;
 
   serializeBinary(): Uint8Array;
-
   toObject(includeInstance?: boolean): UserState.AsObject;
   static toObject(includeInstance: boolean, msg: UserState): UserState.AsObject;
-
-  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-  static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
   static serializeBinaryToWriter(message: UserState, writer: jspb.BinaryWriter): void;
-
   static deserializeBinary(bytes: Uint8Array): UserState;
-
   static deserializeBinaryFromReader(message: UserState, reader: jspb.BinaryReader): UserState;
 }
 
@@ -216,33 +163,22 @@ export namespace UserState {
 
 export class UserDeployerState extends jspb.Message {
   hasAddress(): boolean;
-
   clearAddress(): void;
-
   getAddress(): proto_loom_pb.Address | undefined;
-
   setAddress(value?: proto_loom_pb.Address): void;
 
   clearContractsList(): void;
-
   getContractsList(): Array<DeployerContract>;
-
   setContractsList(value: Array<DeployerContract>): void;
-
   addContracts(value?: DeployerContract, index?: number): DeployerContract;
 
   serializeBinary(): Uint8Array;
-
   toObject(includeInstance?: boolean): UserDeployerState.AsObject;
   static toObject(includeInstance: boolean, msg: UserDeployerState): UserDeployerState.AsObject;
-
-  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-  static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
   static serializeBinaryToWriter(message: UserDeployerState, writer: jspb.BinaryWriter): void;
-
   static deserializeBinary(bytes: Uint8Array): UserDeployerState;
-
   static deserializeBinaryFromReader(message: UserDeployerState, reader: jspb.BinaryReader): UserDeployerState;
 }
 
@@ -255,25 +191,17 @@ export namespace UserDeployerState {
 
 export class GetUserDeployersRequest extends jspb.Message {
   hasUseraddr(): boolean;
-
   clearUseraddr(): void;
-
   getUseraddr(): proto_loom_pb.Address | undefined;
-
   setUseraddr(value?: proto_loom_pb.Address): void;
 
   serializeBinary(): Uint8Array;
-
   toObject(includeInstance?: boolean): GetUserDeployersRequest.AsObject;
   static toObject(includeInstance: boolean, msg: GetUserDeployersRequest): GetUserDeployersRequest.AsObject;
-
-  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-  static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
   static serializeBinaryToWriter(message: GetUserDeployersRequest, writer: jspb.BinaryWriter): void;
-
   static deserializeBinary(bytes: Uint8Array): GetUserDeployersRequest;
-
   static deserializeBinaryFromReader(message: GetUserDeployersRequest, reader: jspb.BinaryReader): GetUserDeployersRequest;
 }
 
@@ -285,25 +213,17 @@ export namespace GetUserDeployersRequest {
 
 export class GetUserDeployersResponse extends jspb.Message {
   clearDeployersList(): void;
-
   getDeployersList(): Array<proto_deployer_whitelist_deployer_whitelist_pb.Deployer>;
-
   setDeployersList(value: Array<proto_deployer_whitelist_deployer_whitelist_pb.Deployer>): void;
-
   addDeployers(value?: proto_deployer_whitelist_deployer_whitelist_pb.Deployer, index?: number): proto_deployer_whitelist_deployer_whitelist_pb.Deployer;
 
   serializeBinary(): Uint8Array;
-
   toObject(includeInstance?: boolean): GetUserDeployersResponse.AsObject;
   static toObject(includeInstance: boolean, msg: GetUserDeployersResponse): GetUserDeployersResponse.AsObject;
-
-  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-  static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
   static serializeBinaryToWriter(message: GetUserDeployersResponse, writer: jspb.BinaryWriter): void;
-
   static deserializeBinary(bytes: Uint8Array): GetUserDeployersResponse;
-
   static deserializeBinaryFromReader(message: GetUserDeployersResponse, reader: jspb.BinaryReader): GetUserDeployersResponse;
 }
 
@@ -315,25 +235,17 @@ export namespace GetUserDeployersResponse {
 
 export class GetDeployedContractsRequest extends jspb.Message {
   hasDeployeraddr(): boolean;
-
   clearDeployeraddr(): void;
-
   getDeployeraddr(): proto_loom_pb.Address | undefined;
-
   setDeployeraddr(value?: proto_loom_pb.Address): void;
 
   serializeBinary(): Uint8Array;
-
   toObject(includeInstance?: boolean): GetDeployedContractsRequest.AsObject;
   static toObject(includeInstance: boolean, msg: GetDeployedContractsRequest): GetDeployedContractsRequest.AsObject;
-
-  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-  static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
   static serializeBinaryToWriter(message: GetDeployedContractsRequest, writer: jspb.BinaryWriter): void;
-
   static deserializeBinary(bytes: Uint8Array): GetDeployedContractsRequest;
-
   static deserializeBinaryFromReader(message: GetDeployedContractsRequest, reader: jspb.BinaryReader): GetDeployedContractsRequest;
 }
 
@@ -345,25 +257,17 @@ export namespace GetDeployedContractsRequest {
 
 export class GetDeployedContractsResponse extends jspb.Message {
   clearContractaddressesList(): void;
-
   getContractaddressesList(): Array<DeployerContract>;
-
   setContractaddressesList(value: Array<DeployerContract>): void;
-
   addContractaddresses(value?: DeployerContract, index?: number): DeployerContract;
 
   serializeBinary(): Uint8Array;
-
   toObject(includeInstance?: boolean): GetDeployedContractsResponse.AsObject;
   static toObject(includeInstance: boolean, msg: GetDeployedContractsResponse): GetDeployedContractsResponse.AsObject;
-
-  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-  static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
   static serializeBinaryToWriter(message: GetDeployedContractsResponse, writer: jspb.BinaryWriter): void;
-
   static deserializeBinary(bytes: Uint8Array): GetDeployedContractsResponse;
-
   static deserializeBinaryFromReader(message: GetDeployedContractsResponse, reader: jspb.BinaryReader): GetDeployedContractsResponse;
 }
 
