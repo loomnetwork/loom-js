1b6d617d1db57d18b99750e5c332379ad99b68cd
suhasagg <suhasagg@gmail.com>
2019-06-14 18:46:33 +0530

    Approve user-deployer-whitelist contract as spender - coin contract call

--- src/tests/e2e/user-deployer-whitelist-tests.ts
+++ src/tests/e2e/user-deployer-whitelist-tests.ts
@@ -11,6 +11,55 @@ import {
 import { createTestHttpClient, waitForMillisecondsAsync } from '../helpers'
 import { B64ToUint8Array } from '../../crypto-utils'
 
+const toCoinE18 = (amount: number): BN => {
+  return new BN(10).pow(new BN(18)).mul(new BN(amount))
+}
+
+async function getClientAndContractCoin(
+  createClient: () => Client
+): Promise<{
+  acct1Client: Client
+  acct1Coin: Contracts.Coin
+  acct1PubKey: Uint8Array
+}> {
+  const acct1PrivKey = B64ToUint8Array(
+    'Hz9P3aHH62mO75A6uMVW3mn0U1KkZSq3t03jfOZfyZxjyJoJctNDY6awaVqOpjCGTjHZZxkc23Z3l39EjLOIFQ=='
+  )
+
+  const acct1PubKey = CryptoUtils.publicKeyFromPrivateKey(acct1PrivKey)
+  const acct1Client = createClient()
+
+  acct1Client.txMiddleware = createDefaultTxMiddleware(acct1Client, acct1PrivKey)
+
+  const acct1Coin = await Contracts.Coin.createAsync(
+    acct1Client,
+    new Address(acct1Client.chainId, LocalAddress.fromPublicKey(acct1PubKey))
+  )
+
+  return { acct1Client, acct1Coin, acct1PubKey, }
+}
+
+async function testBalanceOf(t: test.Test, createClient: () => Client) {
+  const { acct1Client, acct1Coin, acct1PubKey, } = await getClientAndContractCoin(createClient)
+
+  const acct1Owner = new Address(acct1Client.chainId, LocalAddress.fromPublicKey(acct1PubKey))
+  const acct1Balance = await acct1Coin.getBalanceOfAsync(acct1Owner)
+  t.assert(acct1Balance.eq(toCoinE18(10000)), 'Acct 1 balance should be 10000e18')
+  acct1Client.disconnect()
+
+}
+
+async function testApprove(t: test.Test, createClient: () => Client) {
+  const { acct1Client, acct1Coin, acct1PubKey } = await getClientAndContractCoin(createClient)
+
+  const contractAddr = await acct1Client.getContractAddressAsync('user-deployer-whitelist')
+  console.log("Contract Address", contractAddr)
+  await acct1Coin.approveAsync(contractAddr!, toCoinE18(10000))
+
+  acct1Client.disconnect()
+}
+
+
 async function getClientAndContract(
   createClient: () => Client
 ): Promise<{
@@ -65,6 +114,10 @@ async function getTierInfo(t: test.Test, createClient: () => Client) {
 
 test('user-deployer-whitelist', async t => {
   try {
+    await testBalanceOf(t, createTestHttpClient)
+    await waitForMillisecondsAsync(1000)
+    await testApprove(t, createTestHttpClient)
+    await waitForMillisecondsAsync(1000)
     await addUserDeployer(t, createTestHttpClient)
     await waitForMillisecondsAsync(1000)
     await getUserDeployer(t, createTestHttpClient)
