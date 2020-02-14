cf18ced5f77414b8e5594afb7f478c7fc88ee709
Deeprom S <grooveeeeee@gmail.com>
2020-02-14 11:27:57 +0700

    test only one e2e

--- e2e_tests.sh
+++ e2e_tests.sh
@@ -76,7 +76,7 @@ stop_chains() {
 }
 
 run_tests() {
-  yarn test:node
+  # yarn test:node
   # Disabled temporarily because it dies for some unknown reason on Travis
   #yarn test:browser
   yarn e2e:node
