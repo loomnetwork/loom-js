3d4d9a2e534ed6a97856f3e4bb4a95bae32ed2fa
Deeprom S <grooveeeeee@gmail.com>
2020-02-17 17:22:03 +0700

    enable all tests

--- src/tests/e2e_tests.ts
+++ src/tests/e2e_tests.ts
@@ -1,41 +1,41 @@
-// // Client WS-RPC
-// import './e2e/ws-rpc-client-tests'
+// Client WS-RPC
+import './e2e/ws-rpc-client-tests'
 
-// // Loom Provider
-// import './e2e/loom-provider-tests'
-// import './e2e/loom-provider-eth-get-logs'
-// import './e2e/loom-provider-eth-filters'
-// import './e2e/loom-provider-eth-filters-2'
-// import './e2e/loom-provider-subscribe'
-// import './e2e/loom-provider-web3-tests'
-// import './e2e/loom-provider-web3-middlewares-tests'
-// import './e2e/loom-provider-web3-child-events'
+// Loom Provider
+import './e2e/loom-provider-tests'
+import './e2e/loom-provider-eth-get-logs'
+import './e2e/loom-provider-eth-filters'
+import './e2e/loom-provider-eth-filters-2'
+import './e2e/loom-provider-subscribe'
+import './e2e/loom-provider-web3-tests'
+import './e2e/loom-provider-web3-middlewares-tests'
+import './e2e/loom-provider-web3-child-events'
 
-// // EVM
-// import './e2e/client-evm-tests'
-// import './e2e/client-evm-event-tests'
-// import './e2e/client-evm-event-tests-2'
+// EVM
+import './e2e/client-evm-tests'
+import './e2e/client-evm-event-tests'
+import './e2e/client-evm-event-tests-2'
 
-// // Middlewares
-// import './e2e/client-test-tx-cache'
-// import './e2e/client-test-tx-middleware'
-// import './e2e/tron-test-tx-middleware'
-// import './e2e/binance-test-tx-middleware'
+// Middlewares
+import './e2e/client-test-tx-cache'
+import './e2e/client-test-tx-middleware'
+import './e2e/tron-test-tx-middleware'
+import './e2e/binance-test-tx-middleware'
 
-// // Events
-// import './e2e/multiple-events-nd-tests'
+// Events
+import './e2e/multiple-events-nd-tests'
 
-// // Contracts
-// import './e2e/coin-tests'
+// Contracts
+import './e2e/coin-tests'
 import './e2e/address-mapper-tests'
-// // TODO: Re-enable once this is updated to DPOSv2
-// //import './e2e/dpos-tests'
+// TODO: Re-enable once this is updated to DPOSv2
+//import './e2e/dpos-tests'
 
-// // Weave Blueprint Contract
-// import './e2e/contract-tests'
+// Weave Blueprint Contract
+import './e2e/contract-tests'
 
-// // Simple Store Contract
-// import './e2e/evm-contract-tests'
+// Simple Store Contract
+import './e2e/evm-contract-tests'
 
 // Verify hash test
 import './e2e/verify-hash-test'
