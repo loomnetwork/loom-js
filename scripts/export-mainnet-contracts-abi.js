56b4a5071625b332e9b6ea9da1fdac7a936366ae
Vadim Macagon <vadim.macagon@gmail.com>
2020-03-10 19:30:39 +0700

    Fix a couple of issues causing errors TS2307 & TS2416
    The typings generated by TypeChain for the Gateway Solidity contracts
    weren't copied to the dist directory and therefore weren't published in
    the NPM package, this caused compile errors (TS2307) in projects that
    imported loom-js.

--- scripts/export-mainnet-contracts-abi.js
+++ scripts/export-mainnet-contracts-abi.js
@@ -1,9 +1,15 @@
-// This script exports the ABI from the TypeChain generated bindings in src/mainnet-contracts/*.ts
-
 const fs = require('fs');
 const path = require('path');
 
+// Export the ABI from the TypeChain generated bindings in src/mainnet-contracts/*.ts
 fs.appendFileSync(path.join(__dirname, '../src/mainnet-contracts/ERC20Factory.ts'), '\nexport const abi = _abi;');
 fs.appendFileSync(path.join(__dirname, '../src/mainnet-contracts/EthereumGatewayV1Factory.ts'), '\nexport const abi = _abi;');
 fs.appendFileSync(path.join(__dirname, '../src/mainnet-contracts/EthereumGatewayV2Factory.ts'), '\nexport const abi = _abi;');
 fs.appendFileSync(path.join(__dirname, '../src/mainnet-contracts/ValidatorManagerV2Factory.ts'), '\nexport const abi = _abi;');
+
+// Copy the TypeChain generated d.ts files to dist/mainnet-contracts because the TypeScript compiler doesn't
+fs.copyFileSync(path.join(__dirname, '../src/mainnet-contracts/ERC20.d.ts'), path.join(__dirname, '../dist/mainnet-contracts/ERC20.d.ts'));
+fs.copyFileSync(path.join(__dirname, '../src/mainnet-contracts/EthereumGatewayV1.d.ts'), path.join(__dirname, '../dist/mainnet-contracts/EthereumGatewayV1.d.ts'));
+fs.copyFileSync(path.join(__dirname, '../src/mainnet-contracts/EthereumGatewayV2.d.ts'), path.join(__dirname, '../dist/mainnet-contracts/EthereumGatewayV2.d.ts'));
+fs.copyFileSync(path.join(__dirname, '../src/mainnet-contracts/ValidatorManagerV2.d.ts'), path.join(__dirname, '../dist/mainnet-contracts/ValidatorManagerV2.d.ts'));
+fs.copyFileSync(path.join(__dirname, '../src/mainnet-contracts/index.d.ts'), path.join(__dirname, '../dist/mainnet-contracts/index.d.ts'));
