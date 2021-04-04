ac5ace069b7681117d6ee964905269282013cb2b
NightingaleAsh <81185768+NightingaleAsh@users.noreply.github.com>
2021-04-04 20:33:20 +0700

    Add BscTransferGateway (#343)
    * Implement BscTransferGateway
    * Bump version to 1.75.2

---
+++ src/contracts/bsc-gateway.ts
@@ -0,0 +1,43 @@
+import { TransferGateway } from './transfer-gateway'
+import { Client } from '../client'
+import { Address } from '../address'
+import { TransferGatewayWithdrawLoomCoinRequest } from '../proto/transfer_gateway_pb'
+import { marshalBigUIntPB } from '../big-uint'
+import BN from 'bn.js'
+
+export class BscTransferGateway extends TransferGateway {
+  static async createAsync(client: Client, callerAddr: Address): Promise<BscTransferGateway> {
+    const contractAddr = await client.getContractAddressAsync('bsc-gateway')
+    if (!contractAddr) {
+      throw Error('Failed to resolve contract address for BscTransferGateway')
+    }
+
+    return new BscTransferGateway({ contractAddr, callerAddr, client })
+  }
+
+  /**
+   * Sends a request to the DAppChain Gateway to begin withdrawal of LOOM from the current
+   * DAppChain account to an Ethereum account.
+   * @param amount Amount to withdraw.
+   * @param mainnetLoomcoinAddress Ethereum address of LOOM coin contract.
+   * @param recipient Ethereum address of the account the token should be withdrawn to, if this is
+   *                  omitted the Gateway will attempt to use the Address Mapper to retrieve the
+   *                  address of the Ethereum account mapped to the current DAppChain account.
+   * @returns A promise that will be resolved when the DAppChain Gateway has accepted the withdrawal
+   *          request.
+   */
+  withdrawLoomCoinAsync(
+    amount: BN,
+    mainnetLoomcoinAddress: Address,
+    recipient?: Address
+  ): Promise<void> {
+    const req = new TransferGatewayWithdrawLoomCoinRequest()
+    req.setAmount(marshalBigUIntPB(amount))
+    req.setTokenContract(mainnetLoomcoinAddress.MarshalPB())
+    if (recipient) {
+      req.setRecipient(recipient.MarshalPB())
+    }
+
+    return this.callAsync<void>('WithdrawLoomCoin', req)
+  }
+}
