7bb1b5ab784a01948fc9054f1c349353f64f4cd2
Reda <reda.bendiar@gmail.com>
2021-05-12 16:23:54 +0700

    chrome

--- webpack.unit.tests.config.js
+++ webpack.unit.tests.config.js
@@ -36,7 +36,7 @@ module.exports = {
   plugins: [
     // Be default tests will run in Electron, but can use other browsers too,
     // see https://github.com/syarul/webpack-tape-run for plugin settings.
-    new WebpackTapeRun()
+    new WebpackTapeRun("chrome")
   ],
   // silence irrelevant messages
   performance: {
